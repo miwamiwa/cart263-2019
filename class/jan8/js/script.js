19217aa824f674273bdde818f830f2926b9326fb
SAMUEL PARE-CHOUINARD <s_parech@cda131ev5635x.local>
2019-01-08 11:37:46 -0500

    Week1: commented in-class activity

--- class/jan8/js/script.js
+++ class/jan8/js/script.js
@@ -5,6 +5,9 @@ STEP BY STEP CIRCLE EATER
 
 "use strict";
 
+// Global Variables
+
+// avatar
 let player = {
   x: 0,
   y: 0,
@@ -14,6 +17,7 @@ let player = {
   color: "#ff0000"
 }
 
+// food
 let target = {
   x: 0,
   y: 0,
@@ -21,9 +25,13 @@ let target = {
   color: "#0000ff"
 }
 
+// constants
 const HEALTH_LOSS = 1;
 const HEALTH_INCREASE = 50;
 
+// setup()
+//
+// creates canvas object and sets initial food location
 
 function setup(){
 
@@ -32,6 +40,12 @@ function setup(){
   noCursor();
   rectMode(CENTER);
 }
+
+// draw()
+//
+// loops the main game mechanics:
+// updates avatar position and displays avatar and food.
+// checks for avatar/food collisions as long as avatar is alive.
 
 function draw(){
 
@@ -44,16 +58,30 @@ function draw(){
   checkCollisions();
 }
 
+// updateAvatar()
+//
+// assigns player's x, y position to match mouse's position
+// updates and constrains player's size (health).
+// handles death (surprisingly well).
+
 function updateAvatar(){
 
+  // update position
   player.x = mouseX;
   player.y = mouseY;
+
+  // update and constrain size
   player.currentSize -= HEALTH_LOSS;
   player.currentSize = constrain(player.currentSize, 0, player.maxSize);
 
+  // signal avatar's death
   if(player.currentSize ===0)
   player.isAlive = false;
 }
+
+// displayAvatar()
+//
+// assigns fill to the avatar and displays it as an ellipse
 
 function displayAvatar(){
 
@@ -64,6 +92,10 @@ function displayAvatar(){
   pop();
 }
 
+// displayFood()
+//
+// assigns the food's fill and displays it as a rectangle
+
 function displayFood(){
 
   push();
@@ -73,16 +105,27 @@ function displayFood(){
   pop();
 }
 
+// checkCollisions()
+//
+// calculate distance between avatar and food.
+// if they overlap, increase player size and move food.
+
 function checkCollisions(){
 
+  // get distance between avatar and food
   let distance = dist(target.x, target.y, player.x, player.y);
 
-  if(distance <= target.size/2 + player.currentSize){
+  // trigger capture mechanics
+  if(distance <= target.size/2 + player.currentSize/2){
     player.currentSize += HEALTH_INCREASE;
     player.currentSize = constrain(player.currentSize, 0, player.maxSize);
     resetFood();
   }
 }
+
+// resetFood()
+//
+// assign a random location on the canvas to the Food
 
 function resetFood(){
 
